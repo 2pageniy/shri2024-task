e271e1a79f4a8c82d872153949022bcbd505fb32
Vladislav Serov <serov.vladsilaw@yandex.ru>
2024-07-13 00:24:04 +0300

    а

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -173,22 +173,28 @@ export function Main() {
             </div>
 
             <div className="section__panel-wrapper" ref={ref}>
-                {TABS_KEYS.map(key =>
-                    <div role="tabpanel"
-                         key={key}
-                         className={'section__panel' + (key === activeTab ? '' : ' section__panel_hidden')}
-                         aria-hidden={key === activeTab ? 'false' : 'true'} id={`panel_${key}`}
-                         aria-labelledby={`tab_${key}`}>
-                        <ul className="section__panel-list">
-                            {TABS[key].items.map((item, index) =>
-                                <Event
-                                    key={index}
-                                    {...item}
-                                    onSize={onSize}
-                                />
-                            )}
-                        </ul>
-                    </div>
+                {TABS_KEYS.map(key => {
+                        if (key !== activeTab) {
+                            return null;
+                        }
+                        return (
+                            <div role="tabpanel"
+                                 key={key}
+                                 className={'section__panel' + (key === activeTab ? '' : ' section__panel_hidden')}
+                                 aria-hidden={key === activeTab ? 'false' : 'true'} id={`panel_${key}`}
+                                 aria-labelledby={`tab_${key}`}>
+                                <ul className="section__panel-list">
+                                    {TABS[key].items.map((item, index) =>
+                                        <Event
+                                            key={index}
+                                            {...item}
+                                            onSize={onSize}
+                                        />
+                                    )}
+                                </ul>
+                            </div>
+                        )
+                    }
                 )}
                 {hasRightScroll &&
                     <div className="section__arrow" onClick={onArrowCLick}></div>
